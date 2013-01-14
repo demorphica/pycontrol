62ebef63840024fe1c73588e337bb9f8afad287a
demorphica <demorphica@gmail.com>
2013-01-13 16:48:53 -0800

    Create hostcheck.py
    user=sys.argv[1]
    host=sys.argv[2]
    duration=int(sys.argv[3])
    runtime=int(sys.argv[4])
    interval=int(sys.argv[5])
    
    eg. /root/hostcheck.py windowsusername hostname 60 10 5
    
    You can setup cron to run like this for eg.:
    
    */5 * * * * /root/hostcheck.py windowsusername hostname 60 10 5
    
    The above command will start the script every 5 minutes, after starting, the script first checks if the host is alive by sending 2 ICMP packets (ping), if the host is alive, then a 60 second load investigation is done. The load checker will then do a test run by sending 1 wmic requests over ssh each second to the windows PC and then calculates the mean to find the loadavg for 10 seconds. The load checker will sleep for 5 seconds. The load check and sleep cycle will repeat till 60 seconds have passed. Then the aggregate load average is calculated by finding the average of all load check runs. 
    
    If the load average is 1.0% over the entire duration, i.e sys.argv[3], a sleep signal is sent to the remote pc. It is assumed that psshutdown (search for it on a search engine) is available on the path of the remote pc. If the system load average is more than 1.0% then the scripts exits normally
    
    
    The sleep interval (i.e sys.argv[5]) can be tuned to spread the system load check across the duration, (i.e sys.argv[3]) to achieve a better picture of the overall system load across a time without sending too many requests.
    
    Activity logs are available on syslog, so you can run:
    
    tailf /var/log/message
    or tailf -f /var/log messages
    or logread -f 
    
    depending on which linux distro you are on, to see the logs.
    
    needs passwordless ssh to work. Use an SSH server on windows and use openssh (if in Openwrt see: http://wiki.openwrt.org/inbox/replacingdropbearbyopensshserver) because there are passwordless ssh issues with the ssh client and i've tested it only to work with the openssh ssh client. For a windows SSH sever you may try Bitvise SSH server or do an internet search.

---
+++ hostcheck.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import shlex, subprocess, sys, syslog
+from time import time, sleep
+
+user=sys.argv[1]
+host=sys.argv[2]
+duration=int(sys.argv[3])
+runtime=int(sys.argv[4])
+interval=int(sys.argv[5])
+
+syslog.syslog(syslog.LOG_ALERT, "Checking host "+host+" for load status started")
+
+def loadcheck(n):
+        end_time = time() + n
+        systemload = []
+        while time() < end_time:
+                try:
+                        p_ssh = subprocess.Popen(shlex.split("/usr/bin/ssh "+user+"@"+host+" wmic cpu get LoadPercentage"), stdout=subprocess.PIPE)
+                        p_awk = subprocess.Popen( shlex.split('awk "NR==2"'), stdin=p_ssh.stdout, stdout=subprocess.PIPE)
+                        out, err = p_awk.communicate()
+                except ValueError:
+                        out = 0
+                        syslog.syslog(syslog.LOG_ALERT, "loadAvg for host "+host+" could not be retreived, assuming 0%")
+                try:
+                        currentload=float(out)
+                except ValueError:
+                        currentload=0
+                syslog.syslog(syslog.LOG_ALERT, "host "+host+" is "+str(currentload)+"% loaded")
+                systemload.append(currentload)
+                sleep(1)
+        averageload = sum(systemload) / float(len(systemload))
+        return averageload
+
+def pingcheck(name):
+        try:
+                p_ping = subprocess.Popen(shlex.split("/bin/ping -q -c 1 -W 1 -w 2 "+name), stdout=subprocess.PIPE)
+                p_lastrow = subprocess.Popen(shlex.split('/usr/bin/gawk "NR==4"'), stdin=p_ping.stdout, stdout=subprocess.PIPE)
+                p_packetloss = subprocess.Popen(shlex.split("/usr/bin/gawk '{print $7}'"), stdin=p_lastrow.stdout, stdout=subprocess.PIPE)
+                out, err = p_packetloss.communicate()
+                packetloss=filter(type(out).isdigit, out)
+        except ValueError:
+                return 3
+        if int(packetloss) == 100:
+                return 0
+        elif int(packetloss) == 0:
+                return 1
+
+def dohybridsleep(id, name):
+        try:
+                 p_sleep = subprocess.Popen(shlex.split("/usr/bin/ssh "+id+"@"+name+" psshutdown -d -t 01"), stdout=subprocess.PIPE)
+                 out, err = p_sleep.communicate()
+                 return(1)
+        except ValueError:
+                print("Could not determine success for sending sleep signal to host "+host)
+                return(0)
+
+def check(n):
+        end_time = time() + n
+        loadlastduration = []
+        while time() < end_time:
+                load=loadcheck(runtime)
+                syslog.syslog(syslog.LOG_ALERT, "Average load for this test run is "+str(load)+"%")
+                loadlastduration.append(load)
+                sleep(interval)
+        avgloadlastduration=sum(loadlastduration) / float(len(loadlastduration))
+        if avgloadlastduration > 1.0:
+                syslog.syslog(syslog.LOG_ALERT, "Host "+host+" is Working ...")
+                sys.exit()
+        elif avgloadlastduration <= 1.0:
+                syslog.syslog(syslog.LOG_ALERT, "Host "+host+" is Idle")
+                syslog.syslog(syslog.LOG_ALERT, "Sending sleep signal to host "+host)
+                dohybridsleep(user, host)
+                sys.exit()
+        else:
+                syslog.syslog(syslog.LOG_ALERT, "Could not reliably determine load for host "+host)
+        sys.exit()
+
+def main():
+        ishostalive = pingcheck(host)
+        if ishostalive == 3:
+                syslog.syslog(syslog.LOG_ALERT, "ICMP echo request test check for "+host+"failed unexpectedly")
+                sys.exit()
+        elif ishostalive == 0:
+                syslog.syslog(syslog.LOG_ALERT, "Host "+host+" is down")
+                sys.exit()
+        elif ishostalive == 1:
+                check(duration)
+
+if __name__ == '__main__':
+        main()
