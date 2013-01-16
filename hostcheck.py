6464da67da7d9a8ed003a4d8462abe24c49f19d9
demorphica <demorphica@gmail.com>
2013-01-17 02:34:28 +0530

    Update hostcheck.py
    gntp and growl notifications added

--- hostcheck.py
+++ hostcheck.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import shlex, subprocess, sys, syslog
+import gntp.notifier
 from time import time, sleep
 
 username=sys.argv[1]
@@ -10,6 +11,30 @@ runtime=int(sys.argv[4])
 interval=int(sys.argv[5])
 
 #ipaddr, hwaddr, duration, runtime, interval
+def growler(message):
+        try:
+                growl = gntp.notifier.GrowlNotifier(
+                                applicationName = "pycontrol",
+                                notifications = ["Host Event"],
+                                defaultNotifications = ["Host Event"],
+                                hostname = "usern",
+                                password = "passw"
+                        )
+                try:
+                        growl.register()
+                except ValueError:
+                        return 0
+                growl.notify(
+                        noteType = "Host Event",
+                        title = message,
+                        description = "",
+                        icon = "",
+                        sticky = False,
+                        priority = 1,
+                        )
+        except ValueError:
+                syslog.syslog(syslog.LOG_ALERT, "Error in notification subsystem")
+                return 0
 
 class Host:
 
@@ -24,29 +49,32 @@ class Host:
     def pingcheck(self):
 
         try:
-                p_ping = subprocess.Popen(shlex.split("/bin/ping -q -c 1 -W 1 -w 2 "+self.hostname), stdout=subprocess.PIPE)
-                p_lastrow = subprocess.Popen(shlex.split('/usr/bin/gawk "NR==4"'), stdin=p_ping.stdout, stdout=subprocess.PIPE)
-                p_packetloss = subprocess.Popen(shlex.split("/usr/bin/gawk '{print $7}'"), stdin=p_lastrow.stdout, stdout=subprocess.PIPE)
-                out, err = p_packetloss.communicate()
-                packetloss=filter(type(out).isdigit, out)
+            p_ping = subprocess.Popen(shlex.split("/bin/ping -q -c 1 -W 1 -w 2 "+self.hostname), stdout=subprocess.PIPE)
+            p_lastrow = subprocess.Popen(shlex.split('/usr/bin/gawk "NR==4"'), stdin=p_ping.stdout, stdout=subprocess.PIPE)
+            p_packetloss = subprocess.Popen(shlex.split("/usr/bin/gawk '{print $7}'"), stdin=p_lastrow.stdout, stdout=subprocess.PIPE)
+            out, err = p_packetloss.communicate()
+            packetloss=filter(type(out).isdigit, out)
         except ValueError:
-                syslog.syslog(syslog.LOG_ALERT, "ICMP echo request test check for "+self.hostname+"failed unexpectedly")
-                return 3
+            syslog.syslog(syslog.LOG_ALERT, "ICMP echo request test check for "+self.hostname+"failed unexpectedly")
+            growler("ICMP echo request test check for "+self.hostname+"failed unexpectedly")
+            return 3
         if int(packetloss) == 100:
-                syslog.syslog(syslog.LOG_ALERT, "ICMP echo request test resulted in 100% packets lost. Host "+self.hostname+" is down")
-                return 0
+            syslog.syslog(syslog.LOG_ALERT, "ICMP echo request test resulted in 100% packets lost. Host "+self.hostname+" is down")
+            growler("ICMP echo request test resulted in 100% packets lost. Host "+self.hostname+" is down")
+            return 0
         elif int(packetloss) == 0:
-                syslog.syslog(syslog.LOG_ALERT, "ICMP echo request test succeded Host "+self.hostname+" is up")
-                return 1
+            syslog.syslog(syslog.LOG_ALERT, "ICMP echo request test succeded Host "+self.hostname+" is up")
+            growler("ICMP echo request test succeded Host "+self.hostname+" is up")
+            return 1
 
     def sleep(self):
         try:
-                p_sleep = subprocess.Popen(shlex.split("/usr/bin/ssh "+self.username+"@"+self.hostname+" psshutdown -d -t 01"), stdout=subprocess.PIPE)
-                out, err = p_sleep.communicate()
-                return(1)
+            p_sleep = subprocess.Popen(shlex.split("/usr/bin/ssh "+self.username+"@"+self.hostname+" psshutdown -d -t 01"), stdout=subprocess.PIPE)
+            out, err = p_sleep.communicate()
+            return(1)
         except ValueError:
-                print("Could not determine success for sending sleep signal to host "+self.hostname)
-                return(0)
+            growler("Could not determine success for sending sleep signal to host "+self.hostname)
+            return(0)
 
     def wake(self):
         try:
@@ -58,67 +86,78 @@ class Host:
             p_wake = subprocess.Popen(shlex.split("/usr/bin/etherwake -D -i eth0 "+out), stdout=subprocess.PIPE)
             out, err = p_wake.communicate()
             syslog.syslog(syslog.LOG_ALERT, "Sent WoL packet to host"+self.hostname)
+            growler("Sent WoL packet to host"+self.hostname)
             return(1)
         except ValueError:
             syslog.syslog(syslog.LOG_ALERT, "Error sending  WoL packet to host"+self.hostname)
+            growler("Error sending  WoL packet to host"+self.hostname)
             return(0)
 
     def check(self):
         end_time = time() + self.duration
         loadlastduration = []
         while time() < end_time:
-                load=self.loadcheck()
-                syslog.syslog(syslog.LOG_ALERT, "Average load for this test run is "+str(load)+"%")
-                loadlastduration.append(load)
-                syslog.syslog(syslog.LOG_ALERT, "Will resume test run after "+str(self.interval)+" seconds ...")
-                sleep(self.interval)
+            load=self.loadcheck()
+            syslog.syslog(syslog.LOG_ALERT, "Average load for this test run is "+str(load)+"%")
+            growler("Average load for this test run is "+str(load)+"%")
+            loadlastduration.append(load)
+            syslog.syslog(syslog.LOG_ALERT, "Will resume test run after "+str(self.interval)+" seconds ...")
+            growler("Will resume test run after "+str(self.interval)+" seconds ...")
+            sleep(self.interval)
         avgloadlastduration=sum(loadlastduration) / float(len(loadlastduration))
         syslog.syslog(syslog.LOG_ALERT, "Average load for all test runs is "+str(avgloadlastduration)+"%")
+        growler("Average load for all test runs is "+str(avgloadlastduration)+"%")
         return avgloadlastduration
 
     def loadcheck(self):
         end_time = time() + self.runtime
         systemload = []
         while time() < end_time:
-                try:
-                        p_ssh = subprocess.Popen(shlex.split("/usr/bin/ssh "+self.username+"@"+self.hostname+" wmic cpu get LoadPercentage"), stdout=subprocess.PIPE)
-                        p_awk = subprocess.Popen( shlex.split('awk "NR==2"'), stdin=p_ssh.stdout, stdout=subprocess.PIPE)
-                        out, err = p_awk.communicate()
-                except ValueError:
-                        out = 0
-                        syslog.syslog(syslog.LOG_ALERT, "loadAvg for host "+self.hostname+" could not be retreived, assuming 0%")
-                try:
-                        currentload=float(out)
-                except ValueError:
-                        currentload=0
+            try:
+                p_ssh = subprocess.Popen(shlex.split("/usr/bin/ssh "+self.username+"@"+self.hostname+" wmic cpu get LoadPercentage"), stdout=subprocess.PIPE)
+                p_awk = subprocess.Popen( shlex.split('awk "NR==2"'), stdin=p_ssh.stdout, stdout=subprocess.PIPE)
+                out, err = p_awk.communicate()
+            except ValueError:
+                out = 0
+                syslog.syslog(syslog.LOG_ALERT, "loadAvg for host "+self.hostname+" could not be retreived, assuming 0%")
+            try:
+                currentload=float(out)
+            except ValueError:
+                currentload=0
 
-                syslog.syslog(syslog.LOG_ALERT, "host "+self.hostname+" is "+str(currentload)+"% loaded")
-                systemload.append(currentload)
-                sleep(1)
+            syslog.syslog(syslog.LOG_ALERT, "host "+self.hostname+" is "+str(currentload)+"% loaded")
+            systemload.append(currentload)
+            sleep(1)
         averageload = sum(systemload) / float(len(systemload))
         return averageload
+
 
 def main():
 
     host=Host(username, hostname, duration, runtime, interval)
+
     syslog.syslog(syslog.LOG_ALERT, "Checking host "+host.username+" for load status started")
+    growler("Checking host "+host.username+" for load status started")
     ishostalive =host.pingcheck()
+
     if ishostalive == 3:
         sys.exit()
     elif ishostalive == 0:
-        host.wake()
         sys.exit()
     elif ishostalive == 1:
         status=host.check()
         if status > 1.0:
+            growler("Host "+host.hostname+" is Working...")
             sys.exit()
         elif status <= 1.0:
+            growler("Host "+host.username+" has been Idle for "+str(host.duration)+" seconds now")
             syslog.syslog(syslog.LOG_ALERT, "Host "+host.username+" has been Idle for "+str(host.duration)+" seconds now")
-            syslog.syslog(syslog.LOG_ALERT, "There is no demand for host "+host.username+". Sending sleep signal to host "+host.username)
+            syslog.syslog(syslog.LOG_ALERT, "There is no demand for host "+host.hostname+". Sending sleep signal to host "+host.hostname)
             host.sleep()
+            growler("There is no demand for host "+host.hostname+". Sending sleep signal to host "+host.hostname)
             sys.exit()
         else:
-            syslog.syslog(syslog.LOG_ALERT, "Could not reliably determine load for host "+host.username)
+            syslog.syslog(syslog.LOG_ALERT, "Could not reliably determine load for host "+host.hostname)
             sys.exit()
 
 if __name__ == '__main__':
